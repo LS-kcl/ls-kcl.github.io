382a2495e0b839bf2c97acd84c74645fae02b46f
Luke <91289210+LS-kcl@users.noreply.github.com>
2024-03-14 22:27:29 +0000

    Create sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,24 @@
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(
+        !empty($_POST['name'])
+        && !empty($_POST['email'])
+        && !empty($_POST['message'])
+    ){
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $message = $_POST["message"];
+
+
+        $to = "lbmoogs@gmail.com";
+        $subject = "New Submission on Portfolio Website";
+        $body = "Name: {$name}\nEmail: {$email}\nMessage: {$message}";
+        $headers = "From: {$email}";
+
+
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Message sent successfully!";
+        } else {
+            echo "Failed to send message.";
+        }
+    }
+}
